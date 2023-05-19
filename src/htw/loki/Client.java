7a78ba6039c5716545af53e573a2caa7eeaac74d
Diro Baloska <dirobaloska@outlook.com>
2023-05-19 23:33:40 +0200

    working get validMoves

--- src/htw/loki/Client.java
+++ src/htw/loki/Client.java
@@ -36,32 +36,31 @@ public class Client extends Thread{
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("Client " + this.clientNumber + " connecting to " + this.hostname + " with image from .\\image\\image" + (this.clientNumber + 1) + ".png");
-//		try {
-//			this.client = new NetworkClient(this.hostname, "player" + clientNumber, ImageIO.read(new File(".\\image\\image" + (this.clientNumber + 1) + ".png")));
-			final Integer PLAYERNUMBER = 0;
+		try {
+			this.client = new NetworkClient(this.hostname, "player" + clientNumber, ImageIO.read(new File(".\\image\\image" + (this.clientNumber + 1) + ".png")));
+			final Integer PLAYERNUMBER = this.client.getMyPlayerNumber();
 			final Integer[] STONEPOSITIONS = Stone.createInitialStonePosition(PLAYERNUMBER);
 			for(int index = 0; index < STONEPOSITIONS.length; index++) {
 				this.stones[index] = new Stone(STONEPOSITIONS[index], PLAYERNUMBER);
 			}
 			this.move();
-//		} catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+			e.printStackTrace();
+		}
 	}
 	
 	
 	public void move() {
+		long time = System.currentTimeMillis();
 		for(Stone stone : stones) {
 			final Integer[] possibleMoves = stone.getAllPossibleMoves(this.gameboard, this.stones);
-			for(int possibleMove : possibleMoves) {
-				if(stone.hasNeighbour(this.gameboard, this.stones)) System.out.println("Client " + 1 + " stone " + stone.getPosition() + " : " + possibleMove);	
-			}
 //			stone.move(possibleMoves[(new Random()).nextInt(0, possibleMoves.length)]);
 			
 //			if(move is accepted)
 //			this.client.sendMove(new Move(to, from, push));
 		}
+		System.out.println("Player " + this.client.getMyPlayerNumber() + " took " + (System.currentTimeMillis() - time) + " ms to finish processing valid moves");
 //		Integer[] stonePositions = gameboard.getPlayerPositions(this.client.getMyPlayerNumber());
 //		Integer movedStone = (new Random()).nextInt(stonePositions.length);
 //		this.client.sendMove(new Move(stonePositions[movedStone], allPossibleMoves.get((new Random()).nextInt(allPossibleMoves.size())), 0));
--- src/htw/loki/Stone.java
+++ src/htw/loki/Stone.java
@@ -31,7 +31,6 @@ public class Stone {
 	
 	
 	public Integer[] getAllPossibleMoves(GameBoard gameboard, Stone[] stones) {
-//		System.out.println(this.position);
 		Integer[] positions = new Integer[4];
 		for(int index = 0; index < positions.length; index++) {
 			positions[index] = stones[index].getPosition();
@@ -50,69 +49,47 @@ public class Stone {
 		
 		// filter only valid move
 		ArrayList<Integer> validMoves = new ArrayList<Integer>();
-		for(int index = 0; index < allPossibleMoves.size(); index++) {
-			System.out.println("Checking " + allPossibleMoves.get(index));
-			for(Integer position : positions) {
-				System.out.println("Position " + position);
-//				 if self, next
-				if(this.position == position) {
-					System.out.println("Position is self");
-					continue;
-				};
-				
-				// get neighbours of not self position
-				final ArrayList<Integer> neighbours = new ArrayList<Integer>(Arrays.asList(gameboard.getNeighbouringPosition(position)));
-				
-//				if neighbours contains possibleMove, possible move is valid, then next
-				if(neighbours.contains(allPossibleMoves.get(index))) {
-					System.out.println("Neighbour contains " + allPossibleMoves.get(index) + " and is valid");
-					System.out.println("Valid move");
-					continue;
-				}
-				System.out.println("neighbours does not contain " + allPossibleMoves.get(index));
-				
-				ArrayList<Integer> checkPosition = positionList;
-				checkPosition.remove(this.position);
-				for(Integer neighbour : neighbours) {
-					if(neighbour == -1) continue;
-					System.out.println("Neighbour number " + neighbour);
-					if(neighbour == this.position) continue;
-					
-					if(checkPosition.contains(neighbour)) {
-						System.out.println("stone positions contains neighbour: " + neighbour);
-						System.out.println("Valid move");
-						if(!validMoves.contains(allPossibleMoves.get(index))) validMoves.add(allPossibleMoves.get(index));
-						break;
-					};
-					System.out.println("Position " + position + " does not contain " + neighbour);
-					System.out.println("Invalid move detected\n\n");
-				}				
-			}
+		for(Integer move : allPossibleMoves) {
+			if(this.isValidMove(move, gameboard, stones)) validMoves.add(move);
 		}
-		
+				
 		for(Integer validMove : validMoves) {
 			System.out.println("valid moves for stone with position "
 					+ this.position + ", player number "  + this.playerNumber + " is " + validMove);			
 		}
 		
-		
-		return allPossibleMoves.toArray(new Integer[allPossibleMoves.size()]);
+		return validMoves.toArray(new Integer[validMoves.size()]);
 	}
 	
 	
-	public boolean hasNeighbour(GameBoard gameboard, Stone[] stones) {
-		final Integer[] neighbouringPositions = gameboard.getNeighbouringPosition(this.position);
+	public boolean isValidMove(Integer targetPosition, GameBoard gameboard, Stone[] stones) {
+		ArrayList<Integer> positions = new ArrayList<Integer>();
 		for(Stone stone : stones) {
-			if (this == stone) continue;
-			if((new ArrayList<Integer>(Arrays.asList(neighbouringPositions))).contains(stone.getPosition())) return true;
+			if(this == stone) continue;
+			positions.add(stone.position);
 		}
-		return false;
+		
+		for(Integer position : positions) {
+			final ArrayList<Integer> neighbouringPositions = new ArrayList<Integer>(Arrays.asList(gameboard.getNeighbouringPosition(position)));
+			boolean isValid = false;
+			for(Integer neighbourPosition : neighbouringPositions) {
+				if(positions.contains(neighbourPosition)) {
+					isValid = true;
+				}
+			}	
+			
+			if(neighbouringPositions.contains(targetPosition)) isValid = true;
+			if(isValid) continue;
+			return false;
+		}
+		
+		return true;
 	}
 	
 	public static Integer[] createInitialStonePosition(int playerNumber) {
 		switch(playerNumber) {
-			case 0 : return new Integer[] {1, 5, 3, 7};
-			case 1 : return new Integer[] {16, 25, 26, 27};
+			case 0 : return new Integer[] {0, 1, 2, 3};
+			case 1 : return new Integer[] {17, 9, 28, 29};
 			case 2 : return new Integer[] {24, 33, 34, 35};
 			default : return null;
 		}
